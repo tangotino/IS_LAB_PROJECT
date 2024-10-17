038f8ee79e685cb996abc2997be047b460e5f953
tangotino <142896055+tangotino@users.noreply.github.com>
2024-10-17 13:01:29 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB connected');
     } catch (error) {
-        console.error(`Error: ${error.message}`);
+        console.error('Error connecting to MongoDB:', error);
         process.exit(1);
     }
 };

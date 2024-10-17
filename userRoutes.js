8b40279c077073ed56158aa43d2a83ec6ddea419
tangotino <142896055+tangotino@users.noreply.github.com>
2024-10-17 12:52:34 +0530

    Update userRoutes.js

--- userRoutes.js
+++ userRoutes.js
@@ -1,11 +1,26 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/userController');
 const router = express.Router();
+const { registerUser, loginUser, fetchPasswords, savePassword } = require('../controllers/userController'); // Ensure this path is correct
+const {protect} = require('../middleware/authMiddleware'); // Make sure this path is correct
 
-// Route for user registration
-router.post('/register', registerUser);  // Pass directly to the controller
 
-// Route for user login
-router.post('/login', loginUser);  // Pass directly to the controller
+// Register route
+router.post('/register', registerUser);
+
+// Login route
+router.post('/login', loginUser);
+
+console.log('Protect middleware:', protect);
+console.log('Is protect a function?', typeof protect === 'function');
+
+
+// Protect the following routes
+router.use(protect); // Apply the middleware to all subsequent routes
+
+// Fetch passwords route
+router.get('/passwords', fetchPasswords); // Changed to '/passwords'
+
+// Save password route
+router.post('/passwords', savePassword); // Changed to '/passwords'
 
 module.exports = router;

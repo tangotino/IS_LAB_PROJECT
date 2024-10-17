80834f82678b13e9eaa523469291ff12f9e24f29
arjunsanjay <112379011+arjunsanjay@users.noreply.github.com>
2024-10-18 00:06:56 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, fetchPasswords, savePassword } = require('../controllers/userController'); // Ensure this path is correct
-const {protect} = require('../middleware/authMiddleware'); // Make sure this path is correct
-
+const { registerUser, loginUser, verifyTotp, fetchPasswords, savePassword } = require('../controllers/userController'); // Include the verifyTotp function
+const { protect } = require('../middleware/authMiddleware'); // Make sure this path is correct
 
 // Register route
 router.post('/register', registerUser);
 
 // Login route
-router.post('/login', loginUser);
+router.post('/login', loginUser); // Keep this as is, the loginUser function now handles TOTP
+
+// TOTP verification route
+router.post('/verify-totp', verifyTotp); // New route for TOTP verification
 
 console.log('Protect middleware:', protect);
 console.log('Is protect a function?', typeof protect === 'function');
-
 
 // Protect the following routes
 router.use(protect); // Apply the middleware to all subsequent routes
